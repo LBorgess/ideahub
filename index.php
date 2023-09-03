44e57e9a2251bd6e75a13c414ae018e801c84d67
LBorgess <lucas_rancer@hotmail.com>
2023-09-03 13:59:16 -0300

    Listagem das perguntas no index

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use \App\Entity\Pergunta;
+
+// Obtém a listagem das perguntas
+$perguntas = Pergunta::getPergunta();
+
 include __DIR__.'/includes/header.php';
 include __DIR__.'/includes/listagem.php';
 include __DIR__.'/includes/footer.php';
