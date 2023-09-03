e55fce8dfbbaa53c159f0fa0583c1eaf56aefb7b
LBorgess <lucas_rancer@hotmail.com>
2023-09-03 15:58:04 -0300

    Uso da constante do texto para o botão

--- includes/formulario.php
+++ includes/formulario.php
@@ -26,7 +26,7 @@
         </div>
 
         <div class="form-group">
-            <button type="submit" class="btn btn-success">Perguntar</button>
+            <button type="submit" class="btn btn-success"><?=BUTTON?></button>
         </div>
 
     </form>
