bcccbc46fe7074ff511751609aff01e00d8bbdf0
LBorgess <lucas_rancer@hotmail.com>
2023-09-03 15:57:45 -0300

    Criação do aviso de status

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,5 +1,18 @@
 <!-- Responsável por realizar a listagem das perguntas na página -->
 <?php
+
+$mensagem = '';
+if (isset($_GET['status'])){
+    switch($_GET['status']){
+        case 'success':
+            $mensagem = '<div class="alert alert-success">Ação executada com sucesso</div>';
+            break;
+        case 'error':
+            $mensagem = '<div class="alert alert-danger">Ação não executada</div>';
+            break;
+    }
+}
+
 $resultados = '';
 foreach ($perguntas as $pergunta) {
     $resultados .= '<tr>
@@ -7,18 +20,24 @@ foreach ($perguntas as $pergunta) {
                         <td>' . $pergunta->perguntas_conteudo . '</td>
                         <td>' . date('d/m/Y à\s H:i:s', strtotime($pergunta->perguntas_data)) . '</td>
                         <td>
-                            <a href="editar.php?id=' .$pergunta->perguntas_id .'">
+                            <a class="text-light" href="editar.php?id=' . $pergunta->perguntas_id . '">
                                 <button type="button" class="btn btn-primary">Editar</button">
                             <a/>
-                            <a href="excluir.php?id=' .$pergunta->perguntas_id .'">
+                            <a class="text-light" href="excluir.php?id=' . $pergunta->perguntas_id . '">
                                 <button type="button" class="btn btn-danger">Excluir</button">
                             <a/>
                         </td>
                     </tr>';
 }
+
+$resultados = strlen($resultados) ? $resultados : '<tr><td colspan=4 class="text-center">Nenhuma pergunta realizada</td></tr>';
+
 ?>
 
 <main>
+
+    <?=$mensagem?>
+
     <section class="nav justify-content-end">
         <a href="cadastrar.php">
             <button class="btn btn-info">Nova pergunta</button>
