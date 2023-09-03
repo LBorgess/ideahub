19c471ef87760bee6fdcfe9e653b40cffe115afc
LBorgess <lucas_rancer@hotmail.com>
2023-09-03 14:44:03 -0300

    Criação do método atualizar

--- app/Entity/Pergunta.php
+++ app/Entity/Pergunta.php
@@ -60,15 +60,39 @@ class Pergunta
     }
 
     /**
+     * Método responsável por realizar a atualização da pergunta no banco de dados
+     * @return boolean
+     */
+    public function atualizar()
+    {
+        return (new Database('perguntas'))->update('perguntas_id = ' . $this->perguntas_id, [
+            'perguntas_titulo'   => $this->perguntas_titulo,
+            'perguntas_conteudo' => $this->perguntas_conteudo,
+            'perguntas_data'     => $this->perguntas_data
+        ]);
+    }
+
+    /**
      * Método responsável por obter as perguntas no banco de dados
      * @param string $where
      * @param string $order
      * @param string $limit
      * @return array
      */
-    public static function getPergunta($where = null, $order = null, $limit = null)
+    public static function getPerguntas($where = null, $order = null, $limit = null)
     {
         return (new Database('perguntas'))->select($where, $order, $limit)
-        ->fetchAll(PDO::FETCH_CLASS, self::class);
+            ->fetchAll(PDO::FETCH_CLASS, self::class);
+    }
+
+    /**
+     * Método responsável por buscar uma pergunta com base em seu ID
+     * @param integer $id
+     * @return Pergunta
+     */
+    public static function getPergunta($id)
+    {
+        return (new Database('perguntas'))->select('perguntas_id = ' . $id)
+            ->fetchObject(self::class);
     }
 }
